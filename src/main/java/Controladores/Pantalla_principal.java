a52bee6af780b8ca7fa25db588e6706c2d59a412
daniel04lope <daniel04lope@gmail.com>
2024-11-03 20:56:18 +0100

    Desplegable en main terminado

--- src/main/java/Controladores/Pantalla_principal.java
+++ src/main/java/Controladores/Pantalla_principal.java
@@ -2,21 +2,42 @@ package Controladores;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import javafx.scene.control.Button;
 
 public class Pantalla_principal implements Initializable {
     @FXML
-    public Text Username;
+     Text Username;
 
-    private Login login;
+    @FXML
+    private AnchorPane drawer;
 
+    @FXML
+    private Button Desplegable;
+    @FXML
+    private Button Cerrar;
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		System.out.println("Pantalla_principal inicializado correctamente");
-	}
+    private boolean drawerVisible = false;
+    private boolean Cerrardesplegar = false;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // Muestra un mensaje en consola para asegurar que el controlador se cargó correctamente
+        System.out.println("Pantalla_principal inicializado correctamente");
+
+        // Configura el botón para que al hacer clic cambie la visibilidad del drawer
+       
+    }
+
+    public void toggleDrawer() {
+    	System.out.println("Funciona");
+    	Cerrardesplegar=!Cerrardesplegar;
+        drawerVisible = !drawerVisible;
+        Cerrar.setVisible(Cerrardesplegar);
+        drawer.setVisible(drawerVisible);
+    }
+    
 }
